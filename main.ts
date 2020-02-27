90cb6520b44b4ba897857905733711f9585f1e41
MathewMoreno <58610701+MathewMoreno@users.noreply.github.com>
2020-02-27 14:02:20 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+// create an empty array of numbers
+let arrayLoop: number[] = []
+// create a for loop that increments from 0 through 4
+for (let index = 0; index < 4; index++) {
+    arrayLoop[index] = index *2
+    game.splash("This multiplies the index times two: " + arrayLoop[index])
+}
+// return the array loop that multiplies that number by 2
+
+// create a second for loop that increments from o through 4
+
+// display the number and the array number
